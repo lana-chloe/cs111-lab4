77140f3d10d9c38a82b495a3f06e767803a09889
lana-chloe <lanachloe@g.ucla.edu>
2023-03-16 17:03:49 -0700

    Finished lab

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
-## UID: 123456789
+## UID: 105817312
 
 (IMPORTANT: Only replace the above numbers with your true UID, do not modify spacing and newlines, otherwise your tarfile might not be created correctly)
 
 # Hey! I'm Filing Here
 
-One line description of this code.
+A 1 MiB ext2 file system with 2 directories, 1 regular file, and 1 symbolic link.
 
 ## Building
 
-Explain briefly how to build your program.
+To build the program, run
+
+    make clean
+
+    make
 
 ## Running
 
-Show how to compile, mount, and example output of `ls -ain` your mounted
-filesystem.
+Show how to compile, mount, and example output of `ls -ain` your mounted filesystem.
+
+    mkdir mnt
+    sudo mount -o loop cs111-base.img mnt
+    cd mnt
+    ls -ain 
+
+
+total 7
+    2 drwxr -xr-x 3 0 0 1024 .
+                            ..
+    13 lrw-r--r-- 1 1000 1000 11 hello -> hello -world
+    12 -rw-r--r-- 1 1000 1000 12 hello -world
+    11 drwxr -xr-x 2 0 0 1024 lost+found
 
 ## Cleaning up
 
--- ext2-create.c
+++ ext2-create.c
@@ -412,6 +412,7 @@ void write_inode_table(int fd) {
 	permissions. The group and other should only have read permission. Your hello-world file should only be 12
 	bytes long and contain “Hello world” followed by a newline. */
 
+	char const content[] = "Hello world\n";
 	struct ext2_inode hello_world_inode = {0};
 	hello_world_inode.i_mode = EXT2_S_IFREG
 								| EXT2_S_IRUSR
@@ -419,7 +420,7 @@ void write_inode_table(int fd) {
 								| EXT2_S_IRGRP
 								| EXT2_S_IROTH;
 	hello_world_inode.i_uid = 1000;
-	hello_world_inode.i_size = 1024;
+	hello_world_inode.i_size = strlen(content);
 	hello_world_inode.i_atime = current_time;
 	hello_world_inode.i_ctime = current_time;
 	hello_world_inode.i_mtime = current_time;
@@ -439,7 +440,7 @@ void write_inode_table(int fd) {
 						| EXT2_S_IRGRP
 						| EXT2_S_IROTH;
 	hello_inode.i_uid = 1000;
-	hello_inode.i_size =  strlen(target_path);;
+	hello_inode.i_size =  strlen(target_path);
 	hello_inode.i_atime = current_time;
 	hello_inode.i_ctime = current_time;
 	hello_inode.i_mtime = current_time;
